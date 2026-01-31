3309ec2456876ff3e9fe4897bf17415f27dd9807
Douglas Chen <douglas@ichen.ca>
2026-01-31 10:38:18 -0800

    chore: expose lambda to itnernet

--- lib/skill-diff-stack.ts
+++ lib/skill-diff-stack.ts
@@ -2,32 +2,62 @@ import 'dotenv/config';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Duration } from 'aws-cdk-lib/core';
 
 
-export class SkillDiffStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+const env = {
+  OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY || '',
+  REDUCTO_API_KEY: process.env.REDUCTO_API_KEY || '',
+  PIPELINE_ID: process.env.PIPELINE_ID || '',
+  TAVILY_API_KEY: process.env.TAVILY_API_KEY || '',
+}
 
-    // The code that defines your stack goes here
-    
-    const REDUCTO_API_KEY = process.env.REDUCTO_API_KEY || '';
-    const PIPELINE_ID = process.env.PIPELINE_ID || '';
+export class SkillDiffStack extends cdk.Stack {
+  private createSecureProcessResumeLambda() {
     const processResumeLambda = new lambda.Function(this, 'ProcessResumeLambda', {
       code: lambda.Code.fromAsset('lambda/processResume'),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_24_X,
       environment: {
-        REDUCTO_API_KEY,
-        PIPELINE_ID,
+        REDUCTO_API_KEY: env.REDUCTO_API_KEY,
+        PIPELINE_ID: env.PIPELINE_ID,
       },
     });
 
-    const processResumeLambdaUrl = processResumeLambda.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
+    const api = new apigateway.RestApi(this, 'ProcessResumeApi', {
+      restApiName: 'ThrottledPublicService',
+      deployOptions: {
+        stageName: 'prod',
+        throttlingRateLimit: 10,
+        throttlingBurstLimit: 10,
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
     });
 
+    const integration = new apigateway.LambdaIntegration(processResumeLambda);
+    api.root.addMethod('POST', integration);
+  }
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+    const aiLambda = new lambda.Function(this, 'AiLambda', {
+      code: lambda.Code.fromAsset('lambda/ai'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_24_X,
+      timeout: Duration.seconds(60 * 10),
+      environment: {
+        OPENROUTER_API_KEY: env.OPENROUTER_API_KEY,
+      },
+    });
+    
+    this.createSecureProcessResumeLambda();
 
     // const researchCandidateLambda = new lambda.Function(this, 'ResearchCandidateLambda', {
     //   code: lambda.Code.fromAsset('lambda/researchCandidate'),
@@ -47,7 +77,7 @@ export class SkillDiffStack extends cdk.Stack {
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_24_X,
       environment: {
-        TAVILY_API_KEY,
+        TAVILY_API_KEY: env.TAVILY_API_KEY,
       },
       timeout: Duration.seconds(60 * 10),
     });
