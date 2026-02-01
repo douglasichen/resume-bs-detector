91848619d6a790119ba7e1a497acdd9daa7bec9f
Douglas Chen <douglas@ichen.ca>
2026-01-31 16:42:05 -0800

    chore: fix verificaiton

--- lambda/researchCandidateTavily/index.ts
+++ lambda/researchCandidateTavily/index.ts
@@ -55,75 +55,97 @@ export const handler: Handler = async (event, context) => {
   const { questions: allQuestions, fullContent, email, id } = event;
   const questions = allQuestions.slice(0, MAX_QUESTIONS);
 
-  try{
+  try {
+    const apiKey = process.env.TAVILY_API_KEY;
+    if (!apiKey) throw new Error("TAVILY_API_KEY is not set");
+    const client = tavily({ apiKey });
 
-  const apiKey = process.env.TAVILY_API_KEY;
-  if (!apiKey) throw new Error("TAVILY_API_KEY is not set");
-  const client = tavily({ apiKey });
+    const results = questions.map(async (question: string) =>
+      client
+        .search(question, {
+          includeAnswer: "advanced",
+          searchDepth: "advanced",
+          maxResults: 10,
+        })
+        .then(async (res) => {
+          return {
+            question: res.query,
+            answer: res.answer,
+            results: res.results.map((searchRes) => {
+              return {
+                url: searchRes.url,
+                score: searchRes.score,
+              };
+            }),
+          };
+        })
+    );
 
-  const results = questions.map(async (question: string) =>
-    client
-      .search(question, {
-        includeAnswer: "advanced",
-        searchDepth: "advanced",
-        maxResults: 10,
-      })
-      .then(async (res) => {
-        return {
-          question: res.query,
-          answer: res.answer,
-          results: res.results.map((searchRes) => {
-            return {
-              url: searchRes.url,
-              score: searchRes.score,
-            };
+    const awaitedResults = await Promise.all(results);
+    const resultsWithVerification = await Promise.all(
+      awaitedResults.map(async (res) => {
+        const prompt = `Given the following question, answer and source results, determine if the claim is Verified, Unsure, or Bullsh*t. If there is somewhat proof of the claim, it should be Verified (should be most prefered output option). If there is no proof of the claim it should be unsure. If there is a contradiction with the claim, it should be Bullsh*t.`;
+        const result = await ai<{
+          verification: "Verified" | "Unsure" | "Bullsh*t";
+        }>(
+          JSON.stringify({
+            question: res.question,
+            answer: res.answer,
+            results: res.results,
+            prompt,
           }),
+          z.object({
+            verification: z.enum(["Verified", "Unsure", "Bullsh*t"]),
+          })
+        );
+        return {
+          ...res,
+          verification: result.verification,
         };
       })
-  );
+    );
+    console.log(
+      `Verification Results: ${JSON.stringify(
+        resultsWithVerification,
+        null,
+        2
+      )}`
+    );
 
-  const awaitedResults = await Promise.all(results);
-  const resultsWithVerification = await Promise.all(awaitedResults.map(async (res) => {
-    const result = await ai<{ verification: "Verified" | "Unsure" | "Bullsh*t" }>(
-      `Given the following question, answer and source results, determine if the claim is Verified, Unsure, or Bullsh*t. If there is somewhat proof of the claim, it should be Verified (should be most prefered output option). If there is no proof of the claim it should be unsure. If there is a contradiction with the claim, it should be Bullsh*t.`,
-      z.object({
-        verification: z.enum(["Verified", "Unsure", "Bullsh*t"])
+    // create unique db entry to store results to query by user
+    const DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
+    if (!DYNAMODB_TABLE_NAME) throw new Error("DYNAMODB_TABLE_NAME is not set");
+
+    const dynamoDBClient = new DynamoDBClient({ region: "us-east-1" });
+    const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+    await docClient.send(
+      new PutCommand({
+        TableName: DYNAMODB_TABLE_NAME,
+        Item: { id, email, fullContent, results: resultsWithVerification },
       })
     );
-    return {
-      ...res,
-      verification: result.verification,
-    };
-  }));
-  console.log(`Verification Results: ${JSON.stringify(resultsWithVerification, null, 2)}`);
 
-  // create unique db entry to store results to query by user
-  const DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
-  if (!DYNAMODB_TABLE_NAME) throw new Error("DYNAMODB_TABLE_NAME is not set");
+    console.log(`RESULTS STORED IN DYNAMODB at key: ${id}`);
 
-  const dynamoDBClient = new DynamoDBClient({ region: "us-east-1" });
-  const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
-
-  await docClient.send(
-    new PutCommand({
-      TableName: DYNAMODB_TABLE_NAME,
-      Item: { id, email, fullContent, results: resultsWithVerification },
-    })
-  );
-
-  console.log(`RESULTS STORED IN DYNAMODB at key: ${id}`);
-
-
-
-  const domain = "https://resume-bs-detector-frontend.vercel.app";
-  await sendEmail(
-    email,
-    "Your resume has been scanned for bullsh*t!",
-    `<p>Your resume has been processed. You can view the results <a href="${domain}/${id}">here</a>.</p>`
-  );
-
-  } catch(error) {
-    console.error(`Error processing your resume: ${JSON.stringify(error, null, 2)}`);
-    await sendEmail(email, "Error processing your resume", `<p>There was an error processing your resume:\n${JSON.stringify(error, null, 2)}</p>`);
+    const domain = "https://resume-bs-detector-frontend.vercel.app";
+    await sendEmail(
+      email,
+      "Your resume has been scanned for bullsh*t!",
+      `<p>Your resume has been processed. You can view the results <a href="${domain}/${id}">here</a>.</p>`
+    );
+  } catch (error) {
+    console.error(
+      `Error processing your resume: ${JSON.stringify(error, null, 2)}`
+    );
+    await sendEmail(
+      email,
+      "Error processing your resume",
+      `<p>There was an error processing your resume:\n${JSON.stringify(
+        error,
+        null,
+        2
+      )}</p>`
+    );
   }
 };
